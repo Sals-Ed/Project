c478962d5cffb8741d7fb994563926261db0aa5a
Sals-Ed <109129785+Sals-Ed@users.noreply.github.com>
2025-06-15 20:42:14 +0800

    Delete PTTAnalysis_v0 directory

--- PTTAnalysis_v0/environment.py
+++
@@ -1,7 +0,0 @@
-from langchain_ollama import ChatOllama
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
-
-llm = ChatOllama(model="llama3", temperature=0.1)
-chain = llm | StrOutputParser()
